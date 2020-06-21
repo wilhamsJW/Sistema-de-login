d11496e597e6bb6718a490e742a30df854cc4b9f
wilhamsJW <wilhamsmeira@gmail.com>
2020-06-21 10:23:59 -0300

    adding folders

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+
+        <nav class="blue-grey"></nav>
+
+
+
+
+      <!--JavaScript at end of body for optimized loading-->
+      <script type="text/javascript" src="materialize/js/jquery-3.5.1.min.js"></script>
+      <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+
+      <!--Inicialização Jquery-->
+      <script type="text/javascript">
+      $(document).ready(function(){
+
+      }):
+      </script>
+    </body>
+  </html> 
+
